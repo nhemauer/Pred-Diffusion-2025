48af682948a4016ba7cc8b2e8fc3d7aaef55977a
nhemauer <ndhemauer@gmail.com>
2025-06-01 00:15:47 -0400

    6_01_25

--- run_all.bat
+++ run_all.bat
@@ -1,4 +1,4 @@
-jupyter nbconvert --to notebook --execute peha.ipynb --inplace
-jupyter nbconvert --to notebook --execute rf.ipynb --inplace
-jupyter nbconvert --to notebook --execute xgboost.ipynb --inplace
+python -m jupyter nbconvert --to notebook --execute peha.ipynb --inplace
+python -m jupyter nbconvert --to notebook --execute rf.ipynb --inplace
+python -m jupyter nbconvert --to notebook --execute xgboost.ipynb --inplace
 pause
