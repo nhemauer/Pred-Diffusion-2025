6a0ed115528f0ac14187c166722f2095ccb9d677
nhemauer <ndhemauer@gmail.com>
2026-03-02 12:08:32 -0500

    Fixed state bin scripts.

--- ml_state/bin/kreitzer_boehmke_state_xgb_split.sh
+++ ml_state/bin/kreitzer_boehmke_state_xgb_split.sh
@@ -2,13 +2,8 @@
 # launch_all.sh
 # Loops over every .py file in scripts/ and submits a job for each
 
-for script in /storage/work/ndh5286/Projects/Pred_Diffusion_2025/ml_state/*.py; do
+for script in /storage/work/ndh5286/Projects/Pred_Diffusion_2025/ml_policy/kreitzer_boehmke_state_xgb_split.py; do
     name=$(basename "$script" .py)
-
-    # Skip files with "split" in the name
-    if [[ "$name" == *split* ]]; then
-        continue
-    fi
 
     sbatch <<EOF
 #!/bin/bash
@@ -19,12 +14,13 @@ for script in /storage/work/ndh5286/Projects/Pred_Diffusion_2025/ml_state/*.py; 
 #SBATCH --cpus-per-task=48
 #SBATCH --ntasks=1
 #SBATCH --mem=384gb
-#SBATCH --time=336:00:00
+#SBATCH --time=16:00:00
 #SBATCH --job-name=${name}
 #SBATCH --chdir=/storage/work/ndh5286/Projects/Pred_Diffusion_2025
-#SBATCH --output=ml_state/logs/%x_%j.out
+#SBATCH --output=ml_policy/logs/%x_%j.out
+#SBATCH --array=0-49
 
-python "$script"
+python "$script" \$SLURM_ARRAY_TASK_ID
 EOF
 
 done
